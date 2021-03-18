cf431a86ae8cb6b82c69129f205aafef568e0efc
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-03-17 20:02:43 -0500

    Ajustes de login

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def signup_user(request):
+	
+	if request.method == 'GET':
+		return render(request, 'todo/signup_user.html', {'form': UserCreationForm()})
+	else:
+		if request.POST['password1'] == request.POST['password2']:
+			username = request.POST['username']
+			password1 = request.POST['password1']
+			user = User.objects.create_user(username, password=password1)
+			user.save()
+		else:
+			print('El password no corresponde')
