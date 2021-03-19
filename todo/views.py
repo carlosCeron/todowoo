8fb561b4199e56acc0f6cb8819c28991a3605719
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-03-18 21:46:19 -0500

    Logut, login

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,25 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def home(request):
+	return render(request, 'todo/home.html')
+
+
+def login_user(request):
+	if request.method == 'GET':
+		return render(request, 'todo/signup_user.html', {'form': AuthenticationForm()})
+	else:
+		user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+		if user is None:
+			return render(request, 'todo/signup_user.html', {'form': AuthenticationForm(), 'error': 'Autenticacion fallida!'})
+		else:
+			login(request, user)
+			return redirect('current_todos')
+			
 
 
 def signup_user(request):
@@ -26,3 +43,9 @@ def signup_user(request):
 
 def current_todos(request):
 	return render(request, 'todo/current_todos.html')
+
+
+def logout_user(request):
+	if request.method == 'POST':
+		logout(request)
+		return redirect('home')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from todo import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     # Auth
+    path('login/', views.login_user, name='login_user'),
     path('signup/', views.signup_user, name='signup_user'),
+    path('logout/', views.logout_user, name='logout_user'),
     
     # Todos
     path('current/', views.current_todos, name='current_todos'),
